f1a46f0a4d4ac5eef3c130861c06956924cd27f8
MuhammadKarimov-dev <karimovdev01@gmail.com>
2025-02-11 15:23:31 +0500

    first

---
+++ src/views/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Top from './Top'
+import "./style.css"
+
+const Navbar = () => {
+  return (
+    <div className='Navbar'>
+        <Top/>
+    </div>
+  )
+}
+
+export default Navbar
